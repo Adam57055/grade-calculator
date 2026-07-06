f6ebc53b13e2d51d3e7062c54dfdc3f760bab805
Adam57055 <adamtwam85@gmail.com>
2026-07-05 20:16:47 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from openai import OpenAI 
+import json
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model='gpt-5.3-mini',
+    input = [
+        {
+            "role", "system",
+            "content": 
+                "You are a professional grade analyst tasked with assessing the future scores needed for the user to achieve their desired grade.",
+                "Use mathematical equations and verify your solutions to ensure accuracy",
+                "Return with VALID JSON ONLY",
+        },
+    ]
+
+
